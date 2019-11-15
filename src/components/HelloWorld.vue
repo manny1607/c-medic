7fbc70e06851df60c98284a76fe5ce0e2f0f96ee
Emanuel Ledesma <unammannydsma@gmail.com>
2019-11-15 00:33:11 -0600

    se instala y configura vuerouter // se crea layout del login sin funcionalidad

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,144 +1,191 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
     >
-      <v-flex xs12>
+      <div class="d-flex align-center">
         <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
+          alt="Vuetify Logo"
+          class="shrink mr-2"
           contain
-          height="200"
-        ></v-img>
-      </v-flex>
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
 
-      <v-flex
-        mb-5
-        xs12
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
       >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+    <v-content>
+
+      <v-container>
+        <v-layout
+          text-center
+          wrap
+        >
+          <v-flex xs12>
+            <v-img
+              :src="require('../assets/logo.svg')"
+              class="my-3"
+              contain
+              height="200"
+            ></v-img>
+          </v-flex>
+
+          <v-flex mb-4>
+            <h1 class="display-2 font-weight-bold mb-3">
+              Welcome to Vuetify
+            </h1>
+            <p class="subheading font-weight-regular">
+              For help and collaboration with other Vuetify developers,
+              <br>please join our online
+              <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+            </p>
+          </v-flex>
+
+          <v-flex
+            mb-5
+            xs12
           >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+            <h2 class="headline font-weight-bold mb-3">What's next?</h2>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+            <v-layout justify-center>
+              <a
+                v-for="(next, i) in whatsNext"
+                :key="i"
+                :href="next.href"
+                class="subheading mx-3"
+                target="_blank"
+              >
+                {{ next.text }}
+              </a>
+            </v-layout>
+          </v-flex>
 
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
+          <v-flex
+            xs12
+            mb-5
           >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+            <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+            <v-layout justify-center>
+              <a
+                v-for="(link, i) in importantLinks"
+                :key="i"
+                :href="link.href"
+                class="subheading mx-3"
+                target="_blank"
+              >
+                {{ link.text }}
+              </a>
+            </v-layout>
+          </v-flex>
 
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
+          <v-flex
+            xs12
+            mb-5
           >
-            {{ eco.text }}
-          </a>
+            <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+
+            <v-layout justify-center>
+              <a
+                v-for="(eco, i) in ecosystem"
+                :key="i"
+                :href="eco.href"
+                class="subheading mx-3"
+                target="_blank"
+              >
+                {{ eco.text }}
+              </a>
+            </v-layout>
+          </v-flex>
         </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+      </v-container>
+
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  data: () => {
+    return ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/layout/pre-defined',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    })
+  },
+  mounted(){
+    console.log('manny')
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import Router from './router';
+import router from './router.js';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  Router,
+  router,
   render: h => h(App)
 }).$mount('#app')
